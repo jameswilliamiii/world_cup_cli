7af6b5cdce77d24935b485c61a03a84aea9b4e6d
James Stubblefield <jameswilliamiii@gmail.com>
2014-06-16 13:20:50 -0500

    add test classes for standings and standings_scraper

---
+++ test/test_standings.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require 'scores'
+
+class TestStandings < Test::Unit::TestCase
+  def test_header
+
+  end
+
+
+end
---
+++ test/test_standings_scraper.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+require 'scores'
+
+class TestStandingsScraper < Test::Unit::TestCase
+  def test_groups_array
+
+  end
+
+  def test_countries_array
+
+  end
+end
